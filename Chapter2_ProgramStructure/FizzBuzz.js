375f4b0f27011ee9294004d156172beae8d8a66f
Vinu <shrikantsurve98@gmail.com>
2017-07-02 00:36:38 +0530

    Solutions to Chapters 2 and 3

---
+++ Chapter2_ProgramStructure/FizzBuzz.js
@@ -0,0 +1,20 @@
+function FizzBuzz(){
+var i;
+for(i=1;i<=100;i++){
+if(i%3==0){
+if(i%5==0){
+console.log("FizzBuzz");
+}
+else{
+console.log("Fizz");
+}
+}
+else if(i%5==0){
+console.log("Buzz");
+}
+else if{
+console.log(i);    
+}
+}
+}
+FizzBuzz();
--- Chapter3_Functions/Chapter 3 #1.js
+++
@@ -1,8 +0,0 @@
-function min(arg1,arg2)
-{
-    if(arg1<arg2)
-    return arg1;
-    else
-    return arg2;
-}
-console.log(min(3,4));
